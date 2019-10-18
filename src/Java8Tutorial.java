59ba1d4c7e8a60b131dc1afbc6cd50bda9dfe817
MohammadRehan Kodekar <MohdRehan.Kodekar@crossasyst.local>
2019-10-18 16:45:47 +0530

    Explain functional interface

--- src/Java8Tutorial.java
+++ src/Java8Tutorial.java
@@ -29,9 +29,9 @@ import java.util.stream.Collectors;
  * return type
  */
 
-@FunctionalInterface
 interface WhaleJump {
     void jump(String name);
+    void swim(String name);
 }
 
 class Mammal {
@@ -70,8 +70,9 @@ public class Java8Tutorial {
     private static Consumer<String> printer = System.out::println;
     private static Consumer<Integer> printerInt = System.out::println;
 
-    public static List<Integer> total(List<Integer> numbers,  Predicate<Integer> selector)  {
-        return numbers.stream().filter(selector).collect(Collectors.toList());
+
+    public static int total(List<Integer> numbers,  Predicate<Integer> selector)  {
+        return numbers.stream().filter(selector).mapToInt(e -> e).sum();
     }
 
     public static Integer totalEven(List<Integer> numbers)  {
@@ -109,8 +110,41 @@ public class Java8Tutorial {
 //            }
 //        });
 
-        animal.setWhalJump((name) -> System.out.println(name + "  jumps"));
+        animal.setWhalJump(new WhaleJump() {
+            @Override
+            public void jump(String name) {
+
+            }
+
+            @Override
+            public void swim(String name) {
+
+            }
+        });
         System.out.println(total(numbers, integer -> integer % 2 == 0));
         System.out.println(total(numbers, integer -> true));
+
+        ArrayList<Integer> arraylist=new ArrayList<Integer>();
+        arraylist.add(5);
+        arraylist.add(7);
+        arraylist.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+
+        arraylist.forEach(System.out::println);
+
+        arraylist.stream().filter(new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer > 4;
+            }
+        }).collect(Collectors.toList());
+
+
+        List<Integer> numbersGreTHanFOur = arraylist.stream().filter((e-> e > 4)).collect(Collectors.toList());
+        numbersGreTHanFOur.forEach((System.out::println));
     }
 }
